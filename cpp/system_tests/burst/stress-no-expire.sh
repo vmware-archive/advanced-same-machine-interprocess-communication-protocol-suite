71fbe795ac1a9f116b262d43bc9fbd787e964fa2
ratanasov1 <35486720+ratanasov1@users.noreply.github.com>
2022-10-25 11:21:53 +0000

    Fix flaky test on github

--- cpp/system_tests/burst/stress-no-expire.sh
+++ cpp/system_tests/burst/stress-no-expire.sh
@@ -7,7 +7,7 @@ source setup-agent.sh
 # Transfer 50MB 5*500*20K over 64KB ring buffer and 16 readers
 export TORONI_AGENT_RINGBUF_SIZE_KB=64
 export TORONI_AGENT_MESSAGE_SIZE_BYTES=500
-export TORONI_AGENT_BACKPRESSURE_SLEEP_MS=5 # With smaller sleep readers usually expire
+export TORONI_AGENT_BACKPRESSURE_SLEEP_MS=10 # With smaller sleep readers usually expire
 export TORONI_AGENT_EXTRESULT=1
 export TORONI_AGENT_MESSAGES_PER_WRITER=20000
 export TORONI_AGENT_ITERATIONS=10
