64641a965c227d609a10fe2ff36f292b3642c40e
ratanasov1 <35486720+ratanasov1@users.noreply.github.com>
2022-10-27 09:11:11 +0000

    fix

--- java/system_tests/src/main/java/com/vmware/toroni/system_tests/setup-agent.sh
+++ java/system_tests/src/main/java/com/vmware/toroni/system_tests/setup-agent.sh
@@ -2,8 +2,8 @@
 # Copyright 2022 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-P=`readlink -f ../../../../../../../../toroni`
-CLASS_PATH="$P/target/toroni-1.0.jar:$P/target/test-classes:$HOME/.m2/repository/net/java/dev/jna/jna-platform/5.12.1/jna-platform-5.12.1.jar:$HOME/.m2/repository/net/java/dev/jna/jna/5.12.1/jna-5.12.1.jar"
+P=`readlink -f ../../../../../../../..`
+CLASS_PATH="$P/toroni/target/toroni-1.0.jar:$P/system_tests/target/classes:$HOME/.m2/repository/net/java/dev/jna/jna-platform/5.12.1/jna-platform-5.12.1.jar:$HOME/.m2/repository/net/java/dev/jna/jna/5.12.1/jna-5.12.1.jar"
 
 export SYSTEM_TESTS_ROOT=`readlink -f ../../../../../../../../../cpp/system_tests`
 
