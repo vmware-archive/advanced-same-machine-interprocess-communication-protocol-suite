29f4a219b38cbe50d86017c6f6d2ecc2c8edd9f6
ratanasov1 <35486720+ratanasov1@users.noreply.github.com>
2022-10-27 09:42:32 +0000

    fix

--- java/system_tests/src/main/java/com/vmware/toroni/system_tests/setup-agent.sh
+++ java/system_tests/src/main/java/com/vmware/toroni/system_tests/setup-agent.sh
@@ -2,10 +2,11 @@
 # Copyright 2022 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-P=`readlink -f ../../../../../../../..`
-CLASS_PATH="$P/toroni/target/toroni-1.0.jar:$P/system_tests/target/classes:$HOME/.m2/repository/net/java/dev/jna/jna-platform/5.12.1/jna-platform-5.12.1.jar:$HOME/.m2/repository/net/java/dev/jna/jna/5.12.1/jna-5.12.1.jar"
+export TORONI_SRC_ROOT=${TORONI_SRC_ROOT:-"/workspaces/advanced-same-machine-interprocess-communication-protocol-suite"}
 
-export SYSTEM_TESTS_ROOT=`readlink -f ../../../../../../../../../cpp/system_tests`
+CLASS_PATH="$TORONI_SRC_ROOT/java/toroni/target/toroni-1.0.jar:$TORONI_SRC_ROOT/java/system_tests/target/classes:$HOME/.m2/repository/net/java/dev/jna/jna-platform/5.12.1/jna-platform-5.12.1.jar:$HOME/.m2/repository/net/java/dev/jna/jna/5.12.1/jna-5.12.1.jar"
+
+export SYSTEM_TESTS_ROOT=$TORONI_SRC_ROOT/cpp/system_tests
 
 export TORONI_AGENT_DEFAULT="java -cp $CLASS_PATH toroni.system_tests.Agent"
 export TORONI_AGENT_READER=$TORONI_AGENT_DEFAULT
